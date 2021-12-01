007a135c2cb42a92263c2195527c52d9499e62d4
pgogola <gogola.piotr@interia.pl>
2021-12-01 21:13:53 +0100

    Day 1: Sonar Sweep - part 2

--- day-1-sonar-sweep/src/main.rs
+++ day-1-sonar-sweep/src/main.rs
@@ -12,21 +12,31 @@ fn load_data(file_name: &str) -> Vec<u32> {
         .collect()
 }
 
-fn count_depth_increases(data: &Vec<u32>) -> u32 {
-    data.windows(2).fold(
-        0,
-        |acc, value| if value[0] < value[1] { acc + 1 } else { acc },
-    )
+fn count_depth_increases(data: &Vec<u32>, sliding_window_size: usize) -> u32 {
+    data.windows(sliding_window_size)
+        .map(|window| window.iter().sum())
+        .collect::<Vec<_>>()
+        .windows(2)
+        .fold(
+            0,
+            |acc, value: &[u32]| if value[0] < value[1] { acc + 1 } else { acc },
+        )
 }
 
 fn part_1_result(file_name: &str) {
     let data = load_data(file_name);
-    println!("Part 1. Result: {}", count_depth_increases(&data));
+    println!("Part 1. Result: {}", count_depth_increases(&data, 1));
+}
+
+fn part_2_result(file_name: &str) {
+    let data = load_data(file_name);
+    println!("Part 2. Result: {}", count_depth_increases(&data, 3));
 }
 
 fn main() {
     const DATA_FILENAME: &str = "./resources/data.txt";
     part_1_result(DATA_FILENAME);
+    part_2_result(DATA_FILENAME);
 }
 
 #[cfg(test)]
@@ -37,6 +47,13 @@ mod tests {
     fn part_1_test_data() {
         const TEST_DATA_FILENAME: &str = "./resources/test_data.txt";
         let data = load_data(TEST_DATA_FILENAME);
-        assert_eq!(count_depth_increases(&data), 7);
+        assert_eq!(count_depth_increases(&data, 1), 7);
+    }
+
+    #[test]
+    fn part_2_test_data() {
+        const TEST_DATA_FILENAME: &str = "./resources/test_data.txt";
+        let data = load_data(TEST_DATA_FILENAME);
+        assert_eq!(count_depth_increases(&data, 3), 5);
     }
 }
