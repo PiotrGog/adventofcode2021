1eeb0b8d313cde9abe876834d124504b70d79656
pgogola <gogola.piotr@interia.pl>
2021-12-14 19:06:46 +0100

    Day 14: Extended Polymerization - part 2

--- day-14-extended-polymerization/src/main.rs
+++ day-14-extended-polymerization/src/main.rs
@@ -24,6 +24,39 @@ fn load_data(file_name: &str) -> (Vec<char>, HashMap<(char, char), char>) {
     )
 }
 
+fn polymer_to_pairs(polymer: &Vec<char>) -> HashMap<(char, char), usize> {
+    polymer.windows(2).fold(HashMap::new(), |mut acc, chars| {
+        if let Some(val) = acc.get_mut(&(chars[0], chars[1])) {
+            *val+=1;
+        } else {
+            acc.insert((chars[0], chars[1]), 1);
+        }
+        acc
+    })
+}
+
+fn generate_polymer_from_pairs(polymer: HashMap<(char, char), usize>, insertion_rule: &HashMap<(char, char), char>) -> HashMap<(char, char), usize> {
+    let mut result = HashMap::new();
+
+    for (key, value) in polymer {
+        let matching_part = insertion_rule[&key];
+        if let Some(val) = result.get_mut(&(key.0, matching_part)) {
+            *val+=value;
+        } else {
+            result.insert((key.0, matching_part), value);
+        }
+
+        if let Some(val) = result.get_mut(&( matching_part, key.1)) {
+            *val+=value;
+        } else {
+            result.insert((matching_part, key.1), value);
+        }
+    }
+
+    result
+}
+
+#[allow(dead_code)]
 fn generate_polymer(polymer: Vec<char>, insertion_rule: &HashMap<(char, char), char>) -> Vec<char> {
     let mut result = vec![];
     result.reserve(polymer.len() * 2);
@@ -40,6 +73,7 @@ fn generate_polymer(polymer: Vec<char>, insertion_rule: &HashMap<(char, char), c
     result
 }
 
+#[allow(dead_code)]
 fn count_chars(polymer: &Vec<char>) -> HashMap<char, usize> {
     polymer.iter().fold(HashMap::new(), |mut acc, c| {
         if let Some(val) = acc.get_mut(c) {
@@ -51,17 +85,34 @@ fn count_chars(polymer: &Vec<char>) -> HashMap<char, usize> {
     })
 }
 
-fn get_difference_between_most_and_least_common_element(polymer: &Vec<char>) -> usize {
-    let counted_elements =  count_chars(polymer);
+fn count_chars_pairs(polymer: &HashMap<(char, char), usize>) -> HashMap<char, usize> {
+    polymer.iter().fold(HashMap::new(), |mut acc, ((c1, c2), count)| {
+        if let Some(val) = acc.get_mut(c1) {
+            *val += count/2;
+        } else {
+            acc.insert(*c1, count/2);
+        }
+        if let Some(val) = acc.get_mut(c2) {
+            *val+= count/2;
+        } else {
+            acc.insert(*c2, count/2);
+        }
+        acc
+    })
+}
+
+fn get_difference_between_most_and_least_common_element(polymer: &HashMap<(char, char), usize>) -> usize {
+    let counted_elements =  count_chars_pairs(polymer);
     let mut counted_elements = counted_elements.into_iter().collect::<Vec<_>>();
     counted_elements.sort_by(|e1, e2| e2.1.cmp(&e1.1));
     counted_elements.first().unwrap().1 - counted_elements.last().unwrap().1
 }
 
 fn part_1_result(file_name: &str) {
-    let (mut polymer, insertion_rule) = load_data(file_name);
+    let (polymer, insertion_rule) = load_data(file_name);
+    let mut polymer = polymer_to_pairs(&polymer);
     for _ in 0..10 {
-        polymer = generate_polymer(polymer, &insertion_rule);
+        polymer = generate_polymer_from_pairs(polymer, &insertion_rule);
     }
     println!(
         "Part 1. Result: {}",
@@ -69,20 +120,33 @@ fn part_1_result(file_name: &str) {
     );
 }
 
+fn part_2_result(file_name: &str) {
+    let (polymer, insertion_rule) = load_data(file_name);
+    let mut polymer = polymer_to_pairs(&polymer);
+    for _ in 0..40 {
+        polymer = generate_polymer_from_pairs(polymer, &insertion_rule);
+    }
+    println!(
+        "Part 2. Result: {}",
+        get_difference_between_most_and_least_common_element(&polymer)
+    );
+}
+
 fn main() {
     const DATA_FILENAME: &str = "./resources/data.txt";
     part_1_result(DATA_FILENAME);
+    part_2_result(DATA_FILENAME);
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{generate_polymer, load_data, get_difference_between_most_and_least_common_element};
+    use crate::{generate_polymer, load_data, get_difference_between_most_and_least_common_element, polymer_to_pairs, generate_polymer_from_pairs};
 
     #[test]
     fn part_1_test_data() {
         const TEST_DATA_FILENAME: &str = "./resources/test_data.txt";
         let (polymer, insertion_rule) = load_data(TEST_DATA_FILENAME);
-        let mut polymer_org = polymer.clone();
+        let polymer_org = polymer.clone();
         let expected = "NNCB".chars().collect::<Vec<_>>();
         assert_eq!(polymer, expected);
         let expected = "NCNBCHB".chars().collect::<Vec<_>>();
@@ -98,9 +162,22 @@ mod tests {
         let polymer = generate_polymer(polymer, &insertion_rule);
         assert_eq!(polymer, expected);
 
+        let mut polymer = polymer_to_pairs(&polymer_org);
         for _ in 0..10 {
-            polymer_org = generate_polymer(polymer_org, &insertion_rule);
+            polymer = generate_polymer_from_pairs(polymer, &insertion_rule);
         }
-        assert_eq!(get_difference_between_most_and_least_common_element(&polymer_org), 1588);
+        assert_eq!(get_difference_between_most_and_least_common_element(&polymer), 1588);
+    }
+
+    #[test]
+    fn part_2_test_data() {
+        const TEST_DATA_FILENAME: &str = "./resources/test_data.txt";
+        let (polymer, insertion_rule) = load_data(TEST_DATA_FILENAME);
+
+        let mut polymer = polymer_to_pairs(&polymer);
+        for _ in 0..40 {
+            polymer = generate_polymer_from_pairs(polymer, &insertion_rule);
+        }
+        assert_eq!(get_difference_between_most_and_least_common_element(&polymer), 2188189693529);
     }
 }
