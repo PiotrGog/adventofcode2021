e3d3e8e1f48912c18005804de2c937374ff8be8f
piogog <gogola.piotr@interia.pl>
2022-11-13 14:01:09 +0100

    Day 16: Packet Decoder - part 2

--- day-16-packer-decoder/src/main.rs
+++ day-16-packer-decoder/src/main.rs
@@ -1,5 +1,41 @@
-use std::{collections::LinkedList, fs};
+use std::{collections::LinkedList, fs, ops::Shl};
 use to_binary::{self, BinaryString};
+
+trait ExpressionResultTrait {
+    fn get_result(&self) -> usize;
+}
+
+#[repr(u8)]
+#[derive(Debug, Eq, PartialEq)]
+enum PacketType {
+    Sum = 0,
+    Product = 1,
+    Minimum = 2,
+    Maximum = 3,
+    Literal = 4,
+    GreaterThan = 5,
+    LessThan = 6,
+    EqualTo = 7,
+}
+
+impl From<u8> for PacketType {
+    fn from(item: u8) -> Self {
+        match item {
+            0u8 => Self::Sum,
+            1u8 => Self::Product,
+            2u8 => Self::Minimum,
+            3u8 => Self::Maximum,
+            4u8 => Self::Literal,
+            5u8 => Self::GreaterThan,
+            6u8 => Self::LessThan,
+            7u8 => Self::EqualTo,
+            unsupported_value => panic!(
+                "Value {} cannot be converted to PacketType",
+                unsupported_value
+            ),
+        }
+    }
+}
 
 #[derive(Debug, Eq, PartialEq)]
 enum Packet {
@@ -18,15 +54,26 @@ impl Packet {
         let packet_data_and_rest = &data[6..];
         let packet_version = u8::from_str_radix(packet_version, 2).unwrap();
         let type_id = u8::from_str_radix(type_id, 2).unwrap();
+        let packet_and_rest =
+            Self::create_packet_and_return_rest(type_id, packet_version, packet_data_and_rest);
+        Some(packet_and_rest)
+    }
 
-        let packet_and_rest = match type_id {
-            4u8 => {
+    fn create_packet_and_return_rest(
+        type_id: u8,
+        packet_version: u8,
+        packet_data_and_rest: &str,
+    ) -> (Packet, &str) {
+        match PacketType::from(type_id) {
+            PacketType::Literal => {
                 Self::create_literal_packet_and_return_rest(packet_version, packet_data_and_rest)
             }
-            _ => Self::create_operator_packet_and_return_rest(packet_version, packet_data_and_rest),
-        };
-
-        Some(packet_and_rest)
+            operator_packet_type => Self::create_operator_packet_and_return_rest(
+                operator_packet_type,
+                packet_version,
+                packet_data_and_rest,
+            ),
+        }
     }
 
     fn create_literal_packet_and_return_rest(
@@ -55,6 +102,7 @@ impl Packet {
     }
 
     fn create_operator_packet_and_return_rest(
+        operator_packet_type: PacketType,
         packet_version: u8,
         packet_data_and_rest: &str,
     ) -> (Packet, &str) {
@@ -69,7 +117,11 @@ impl Packet {
         };
 
         (
-            Packet::Operator(OperatorPacket::new(packet_version, subpackets)),
+            Packet::Operator(OperatorPacket::new(
+                packet_version,
+                operator_packet_type,
+                subpackets,
+            )),
             rest,
         )
     }
@@ -114,6 +166,13 @@ impl Packet {
 
         (subpackets, subpackets_and_rest)
     }
+
+    fn get_internal(&self) -> &dyn ExpressionResultTrait {
+        match self {
+            Self::Literal(x) => x,
+            Self::Operator(x) => x,
+        }
+    }
 }
 
 #[derive(Debug, Eq, PartialEq)]
@@ -128,17 +187,102 @@ impl LiteralPacket {
     }
 }
 
+impl ExpressionResultTrait for LiteralPacket {
+    fn get_result(&self) -> usize {
+        self.value
+            .iter()
+            .rev()
+            .enumerate()
+            .fold(0, |acc, (shift, x)| acc | (*x as usize).shl(4 * shift))
+    }
+}
+
 #[derive(Debug, Eq, PartialEq)]
 struct OperatorPacket {
     version: u8,
+    operator_type: PacketType,
     subpackets: Vec<Packet>,
 }
 
 impl OperatorPacket {
-    pub fn new(version: u8, subpackets: Vec<Packet>) -> Self {
+    pub fn new(version: u8, operator_type: PacketType, subpackets: Vec<Packet>) -> Self {
         Self {
             version,
+            operator_type,
             subpackets,
+        }
+    }
+
+    fn sum(&self) -> usize {
+        self.subpackets
+            .iter()
+            .fold(0, |acc, x| acc + x.get_internal().get_result())
+    }
+
+    fn product(&self) -> usize {
+        self.subpackets
+            .iter()
+            .fold(1, |acc, x| acc * x.get_internal().get_result())
+    }
+
+    fn minimum(&self) -> usize {
+        self.subpackets
+            .iter()
+            .map(|x| x.get_internal().get_result())
+            .min()
+            .unwrap()
+    }
+
+    fn maximum(&self) -> usize {
+        self.subpackets
+            .iter()
+            .map(|x| x.get_internal().get_result())
+            .max()
+            .unwrap()
+    }
+
+    fn greater_than(&self) -> usize {
+        if self.subpackets[0].get_internal().get_result()
+            > self.subpackets[1].get_internal().get_result()
+        {
+            1
+        } else {
+            0
+        }
+    }
+
+    fn less_than(&self) -> usize {
+        if self.subpackets[0].get_internal().get_result()
+            < self.subpackets[1].get_internal().get_result()
+        {
+            1
+        } else {
+            0
+        }
+    }
+
+    fn equal_to(&self) -> usize {
+        if self.subpackets[0].get_internal().get_result()
+            == self.subpackets[1].get_internal().get_result()
+        {
+            1
+        } else {
+            0
+        }
+    }
+}
+
+impl ExpressionResultTrait for OperatorPacket {
+    fn get_result(&self) -> usize {
+        match self.operator_type {
+            PacketType::Sum => self.sum(),
+            PacketType::Product => self.product(),
+            PacketType::Minimum => self.minimum(),
+            PacketType::Maximum => self.maximum(),
+            PacketType::GreaterThan => self.greater_than(),
+            PacketType::LessThan => self.less_than(),
+            PacketType::EqualTo => self.equal_to(),
+            _ => panic!(),
         }
     }
 }
@@ -173,6 +317,7 @@ impl BITSTransmision {
                 Packet::Literal(LiteralPacket { version, .. }) => version_sum += *version as usize,
                 Packet::Operator(OperatorPacket {
                     version,
+                    operator_type: _,
                     subpackets,
                 }) => {
                     version_sum += *version as usize;
@@ -184,6 +329,14 @@ impl BITSTransmision {
         }
 
         Some(version_sum)
+    }
+
+    pub fn evaluate(&self) -> Option<usize> {
+        if let Some(packet) = self.packet.as_ref() {
+            Some(packet.get_internal().get_result())
+        } else {
+            None
+        }
     }
 }
 
@@ -200,9 +353,16 @@ fn part_1_result(file_name: &str) {
     );
 }
 
+fn part_2_result(file_name: &str) {
+    let bits_transmission_data = load_file(file_name);
+    let bits_transmission = BITSTransmision::from_hex_string(&bits_transmission_data);
+    println!("Part 2. Result: {}", bits_transmission.evaluate().unwrap());
+}
+
 fn main() {
     const DATA_FILENAME: &str = "./resources/data.txt";
     part_1_result(DATA_FILENAME);
+    part_2_result(DATA_FILENAME);
 }
 
 #[cfg(test)]
@@ -229,6 +389,18 @@ mod tests {
     }
 
     #[test]
+    fn literal_packet_get_expression_value() {
+        assert_eq!(
+            LiteralPacket {
+                version: 6,
+                value: vec![0b0111, 0b1110, 0b0101]
+            }
+            .get_result(),
+            2021
+        );
+    }
+
+    #[test]
     fn packet_create_return_operator_with_length_type_id_0() {
         let (result_packet, rest) =
             Packet::create("00111000000000000110111101000101001010010001001000000000").unwrap();
@@ -236,6 +408,7 @@ mod tests {
             result_packet,
             Packet::Operator(OperatorPacket {
                 version: 1,
+                operator_type: PacketType::from(0b110),
                 subpackets: vec![
                     Packet::Literal(LiteralPacket {
                         version: 6,
@@ -259,6 +432,7 @@ mod tests {
             result_packet,
             Packet::Operator(OperatorPacket {
                 version: 7,
+                operator_type: PacketType::from(0b011),
                 subpackets: vec![
                     Packet::Literal(LiteralPacket {
                         version: 2,
@@ -304,5 +478,61 @@ mod tests {
         let bits_transmission_data = "A0016C880162017C3686B18A3D4780";
         let bits_transmission = BITSTransmision::from_hex_string(bits_transmission_data);
         assert_eq!(bits_transmission.sum_up_versions().unwrap(), 31);
+    }
+
+    #[test]
+    fn evaluate_expression_sum() {
+        let bits_transmission_data = "C200B40A82";
+        let bits_transmission = BITSTransmision::from_hex_string(bits_transmission_data);
+        assert_eq!(bits_transmission.evaluate().unwrap(), 3);
+    }
+
+    #[test]
+    fn evaluate_expression_product() {
+        let bits_transmission_data = "04005AC33890";
+        let bits_transmission = BITSTransmision::from_hex_string(bits_transmission_data);
+        assert_eq!(bits_transmission.evaluate().unwrap(), 54);
+    }
+
+    #[test]
+    fn evaluate_expression_minimum() {
+        let bits_transmission_data = "880086C3E88112";
+        let bits_transmission = BITSTransmision::from_hex_string(bits_transmission_data);
+        assert_eq!(bits_transmission.evaluate().unwrap(), 7);
+    }
+
+    #[test]
+    fn evaluate_expression_maximum() {
+        let bits_transmission_data = "CE00C43D881120";
+        let bits_transmission = BITSTransmision::from_hex_string(bits_transmission_data);
+        assert_eq!(bits_transmission.evaluate().unwrap(), 9);
+    }
+
+    #[test]
+    fn evaluate_expression_less_than() {
+        let bits_transmission_data = "D8005AC2A8F0";
+        let bits_transmission = BITSTransmision::from_hex_string(bits_transmission_data);
+        assert_eq!(bits_transmission.evaluate().unwrap(), 1);
+    }
+
+    #[test]
+    fn evaluate_expression_greater_than() {
+        let bits_transmission_data = "F600BC2D8F";
+        let bits_transmission = BITSTransmision::from_hex_string(bits_transmission_data);
+        assert_eq!(bits_transmission.evaluate().unwrap(), 0);
+    }
+
+    #[test]
+    fn evaluate_expression_equal_to() {
+        let bits_transmission_data = "9C005AC2F8F0";
+        let bits_transmission = BITSTransmision::from_hex_string(bits_transmission_data);
+        assert_eq!(bits_transmission.evaluate().unwrap(), 0);
+    }
+
+    #[test]
+    fn evaluate_expression_1_plus_3_equal_to_2_multiple_2() {
+        let bits_transmission_data = "9C0141080250320F1802104A08";
+        let bits_transmission = BITSTransmision::from_hex_string(bits_transmission_data);
+        assert_eq!(bits_transmission.evaluate().unwrap(), 1);
     }
 }
